2fcbb957d10fdc8b579ffd31269a3123c1e880d9
hyalamat <hyalamat@cisco.com>
2019-04-13 10:13:10 +0530

    Http understand

---
+++ src/ex/Card.java
@@ -0,0 +1,12 @@
+package ex;
+import java.io.*;
+
+public class Card {
+public static void main(String args[])
+{
+	File dir=new File("E:\\hari");
+	dir.mkdir();
+	
+	System.out.println("created");
+}
+}
